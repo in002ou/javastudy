4f540dc0c3fed7780af240639565a86439ae4f7f
in002ou <in002ou@gmail.com>
2023-01-18 19:50:36 +0900

    2023-01-18END

---
+++ 05_Inheritance/src/practice05_Cart/Meat.java
@@ -0,0 +1,13 @@
+package practice05_Cart;
+
+public class Meat extends Product {
+
+	// new Meat(1, "소고기", 5000)
+	
+	public Meat(int prodNo, String prodName, int prodPrice) {
+		super(prodNo, prodName, prodPrice);	// () 열어주면 생성자가 된다, 상위 클래스에 저장하라.
+	}
+	
+	
+	
+}
