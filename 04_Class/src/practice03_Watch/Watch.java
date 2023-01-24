f97832abe47ba9c2cfc67b6af0de24864cc9378e
in002ou <in002ou@gmail.com>
2023-01-24 22:54:39 +0900

    124

--- 04_Class/src/practice03_Watch/Watch.java
+++ 04_Class/src/practice03_Watch/Watch.java
@@ -2,45 +2,47 @@ package practice03_Watch;
 
 public class Watch {
 
-		// 필드
-		private int hour;  // 0 ~ 23
-		private int minute;  // 0 ~ 59
-		private int second;  // 0 ~ 59
-		
-		// 생성자
-		public Watch(int hour, int minute, int second) {
-			this.hour = hour;
-			this.minute = minute;
-			this.second = second;
+	// 필드
+	private int hour;  // 0 ~ 23
+	private int minute;  // 0 ~ 59
+	private int second;  // 0 ~ 59
+	
+	// 생성자
+	public Watch(int hour, int minute, int second) {
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	
+	// 메소드
+	public void addHour(int hour) {
+		if(hour <= 0) {
+			return;
 		}
+		this.hour += hour;
+		this.hour %= 24;  // hour를 항상 0 ~ 23의 범위로 만들어 주는 코드
+	}
+	
+	public void addMinute(int minute) {
+		if(minute <= 0) {
+			return;
+		}
+		this.minute += minute;
+		addHour(this.minute / 60);  // 몇 시간이 추가되었는지 처리하는 코드
+		this.minute %= 60;  // minute을 항상 0 ~ 59의 범위로 만들어 주는 코드
+	}
+	
+	public void addSecond(int second) {
+		if(second <= 0) {
+			return;
+		}
+		this.second += second;
+		addMinute(this.second / 60);  // 몇 분이 추가되었는지 처리하는 코드
+		this.second %= 60;  // second을 항상 0 ~ 59의 범위로 만들어 주는 코드
+	}
+	
+	public void see() {
+		System.out.println(hour + "시 " + minute + "분 " + second + "초");
+	}
 
-		// 메소드
-		public void addHour(int hour) {
-			this.hour += hour;
-			if(this.hour >= 24) {
-				this.hour %= 24;
-			} 
-		}
-		public void addMinute(int minute) {
-			this.minute += minute;
-			if(this.minute >= 60) {
-				this.minute %= 60;
-				hour += this.minute / 60;
-			}
-		}
-		public void addSecond(int second) {
-			this.second += second;
-			if(this.second >= 60) {
-				this.second %= 60;
-				minute += second / 60;
-			} 
-			if(minute >= 60) {
-				minute %= 60;
-				hour++;
-			}
-		}
-		
-		public void see() {
-			System.out.println(hour + "시 " + minute + "분 " + second + "초");
-		}
 }
